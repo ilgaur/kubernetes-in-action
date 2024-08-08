28c728a3e3916dee0e0b0ce371e712f2f4f69fd9
ilgardadashi <ilgardadashi@gmail.com>
2024-08-08 15:03:36 +0330

    made a mistake in response log to demonstrate v2 image, fixed now

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const http = require('http');
 const os = require('os');
 console.log("Kubia server starting...");
 var handler = function(request, response) {
-  console.log("(v2) Received request from " + request.connection.remoteAddress);
+  console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
+  response.end("(v2) You've hit " + os.hostname() + "\n");
 };
 var www = http.createServer(handler);
 www.listen(8080);
