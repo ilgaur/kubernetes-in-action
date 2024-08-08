0533f63342413c9f949ab00d47041296c3a956e9
ilgardadashi <ilgardadashi@gmail.com>
2024-08-09 01:49:32 +0330

    changing the app to throw 500 on more than 5 requests to demonstrate how the liveness probe works, pushed v3 image, change kubectl deployments settings to add live probbing

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const os = require('os');
 console.log("Kubia server starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
-  response.writeHead(200);
-  response.end("(v2) You've hit " + os.hostname() + "\n");
+  requestCount++;
+  if (requestCount > 5) {
+    response.writeHead(500);
+    response.end("internal server error");
+  } else {
+    response.writeHead(200);
+    response.end("(v3) You've hit " + os.hostname() + "\n")
+  }
 };
 var www = http.createServer(handler);
 www.listen(8080);
